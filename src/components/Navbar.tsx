69bb3206e3bf1b56307e7c15ee1cbb6e2a694510
meetanupam <theanupamshakya@gmail.com>
2024-11-14 16:40:17 +0530

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,86 @@
+// Navbar.tsx
+
+import Link from "next/link";
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar flex bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <button tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </button>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <details>
+                  <summary>Parent</summary>
+                  <ul className="p-2">
+                    <li>
+                      <Link href="/submenu1">Submenu 1</Link>
+                    </li>
+                    <li>
+                      <Link href="/submenu2">Submenu 2</Link>
+                    </li>
+                  </ul>
+                </details>
+              </li>
+              <li>
+                <Link href="/item3">Item 3</Link>
+              </li>
+            </ul>
+          </div>
+          <Link href="/" className="btn btn-ghost text-xl">
+            daisyUI
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <details>
+                <summary>Parent</summary>
+                <ul className="p-2">
+                  <li>
+                    <Link href="/submenu1">Submenu 1</Link>
+                  </li>
+                  <li>
+                    <Link href="/submenu2">Submenu 2</Link>
+                  </li>
+                </ul>
+              </details>
+            </li>
+            <li>
+              <Link href="/item3">Item 3</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <button className="btn">Button</button>
+        </div>
+      </nav>
+    </div>
+  );
+}
